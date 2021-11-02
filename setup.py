e75e1a7f5b3af0c1dd931bfa6b2562423178780f
TheAwiteb <59842932+TheAwiteb@users.noreply.github.com>
2021-11-02 22:49:55 +0300

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 KEYWORD = ["fatoora", "Fatoora", "ZATCA", "QR-Code"]
 
-version = "1.0.1"
+version = "1.1.0"
 
 with open("README.md", "r", encoding="utf-8") as readme_file:
     long_description = readme_file.read()
