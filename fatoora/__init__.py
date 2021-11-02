7311b1b3316d75c7f873ce48e75208927a2125d2
TheAwiteb <59842932+TheAwiteb@users.noreply.github.com>
2021-11-02 19:55:40 +0300

    add version file

--- fatoora/__init__.py
+++ fatoora/__init__.py
@@ -1,1 +1,4 @@
 from .fatoora import Fatoora
+from .version import version, version_info
+
+__all__ = "Fatoora", "version", "version_info"
---
+++ fatoora/version.py
@@ -0,0 +1,17 @@
+__all__ = "version", "version_info"
+
+version = "1.0.1"
+
+
+def version_info() -> str:
+    import platform
+    import sys
+    from pathlib import Path
+
+    info = {
+        "fatoora version": version,
+        "install path": Path(__file__).resolve().parent,
+        "python version": sys.version,
+        "platform": platform.platform(),
+    }
+    return "\n".join(f"{key}:{val}" for key, val in info.items())
