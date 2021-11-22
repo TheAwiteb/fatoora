abf9e0648818f156b50ff04abf3515346dc5217a
TheAwiteb <59842932+TheAwiteb@users.noreply.github.com>
2021-11-22 18:24:13 +0300

    add tests of  QR code url

--- tests/fatoora_test.py
+++ tests/fatoora_test.py
@@ -3,6 +3,7 @@ from json import loads
 import os
 
 qrcode_filename = "qr_code.png"
+qrcode_filename_with_url = "qr_code_url.png"
 
 fatoora_obj = Fatoora(
     seller_name="Awiteb",
@@ -10,6 +11,15 @@ fatoora_obj = Fatoora(
     invoice_date=1635872693.3186214,  # timestamp
     total_amount=100,  # or 100.0, 100.00, "100.0", "100.00"
     tax_amount=15,  # or 15.0, 15.00, "15.0", "15.00"
+)
+
+fatoora_obj_with_url = Fatoora(
+    seller_name="Awiteb",
+    tax_number=1234567891,  # or "1234567891"
+    invoice_date=1635872693.3186214,  # timestamp
+    total_amount=100,  # or 100.0, 100.00, "100.0", "100.00"
+    tax_amount=15,  # or 15.0, 15.00, "15.0", "15.00"
+    qrcode_url="https://example.com"
 )
 
 fatoora_details = {
@@ -22,51 +32,69 @@ fatoora_details = {
 
 
 def test_seller_name():
-    assert fatoora_obj.seller_name == fatoora_details.get("seller_name")
+    for obj in [fatoora_obj, fatoora_obj_with_url]:
+        assert obj.seller_name == fatoora_details.get("seller_name")
 
 
 def test_tax_number():
-    assert fatoora_obj.tax_number == fatoora_details.get("tax_number")
+    for obj in [fatoora_obj, fatoora_obj_with_url]:
+        assert obj.tax_number == fatoora_details.get("tax_number")
 
 
 def test_invoice_date():
-    assert fatoora_obj.tags[3] == fatoora_details.get("invoice_date")
+    for obj in [fatoora_obj, fatoora_obj_with_url]:
+        assert obj.tags[3] == fatoora_details.get("invoice_date")
 
 
 def test_total_amount():
-    assert fatoora_obj.total_amount == fatoora_details.get("total_amount")
+    for obj in [fatoora_obj, fatoora_obj_with_url]:
+        assert obj.total_amount == fatoora_details.get("total_amount")
 
 
 def test_tax_amount():
-    assert fatoora_obj.tax_amount == fatoora_details.get("tax_amount")
+    for obj in [fatoora_obj, fatoora_obj_with_url]:
+        assert obj.tax_amount == fatoora_details.get("tax_amount")
 
 
 def test_dict():
-    assert fatoora_obj.dict() == fatoora_details
+    for obj in [fatoora_obj, fatoora_obj_with_url]:
+        assert obj.dict() == fatoora_details
 
 
 def test_json():
     json = fatoora_obj.json()
+    json_with_url = fatoora_obj_with_url.json()
+    json_dict_with_url = loads(json_with_url)
     json_dict = loads(json)
 
-    json_dict.get("seller_name") == fatoora_details.get("seller_name")
-    json_dict.get("tax_number") == fatoora_details.get("tax_number")
-    json_dict.get("invoice_date") == fatoora_details.get("invoice_date")
-    json_dict.get("total_amount") == fatoora_details.get("total_amount")
-    json_dict.get("tax_amount") == fatoora_details.get("tax_amount")
+    for dct in [json_dict, json_dict_with_url]:
+        assert dct.get("seller_name") == fatoora_details.get("seller_name")
+        assert dct.get("tax_number") == fatoora_details.get("tax_number")
+        assert dct.get("invoice_date") == fatoora_details.get("invoice_date")
+        assert dct.get("total_amount") == fatoora_details.get("total_amount")
+        assert dct.get("tax_amount") == fatoora_details.get("tax_amount")
 
 
 def test_qrcode():
     fatoora_obj.qrcode(qrcode_filename)
-    assert os.path.lexists(qrcode_filename)
+    fatoora_obj_with_url.qrcode(qrcode_filename_with_url)
+    for filename in [qrcode_filename, qrcode_filename_with_url]:
+        assert os.path.lexists(filename)
 
 
 def test_read_qrcode():
     dct = Fatoora.read_qrcode(qrcode_filename, dct=True)
     base = Fatoora.read_qrcode(qrcode_filename, dct=False)
+    
+    url = Fatoora.read_qrcode(qrcode_filename_with_url, dct=False)
+    
+    assert url == fatoora_obj_with_url.qrcode_url
+    assert base == fatoora_obj_with_url.base64
+
     assert dct == fatoora_details
     assert base == fatoora_obj.base64
 
 
 def test_base2dict():
-    assert Fatoora.base2dict(fatoora_obj.base64) == fatoora_details
+    for obj in [fatoora_obj, fatoora_obj_with_url]:
+        assert Fatoora.base2dict(obj.base64) == fatoora_details
